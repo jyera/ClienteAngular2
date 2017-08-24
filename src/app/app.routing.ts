5155914ebab5bff7b9636f9b5330c304c75371ab
SrPepperoni <jaimeyera@gmail.com>
2017-08-24 19:49:38 +0200

    commit clase
    clase 90

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -13,13 +13,16 @@ import { ArtistAddComponent } from './components/artist-add.component';
 
 import { ArtistEditComponent } from './components/artist-edit.component';
 
+import { ArtistDetailComponent } from './components/artist-detail.component';
+
 const appRoutes: Routes = [
 	{path: '', component: HomeComponent},
 	{path: 'artistas/:page', component: ArtistListComponent},
 	{path: 'crear-artista', component: ArtistAddComponent},
 	{path: 'editar-artista/:id', component: ArtistEditComponent},
+	{path: 'artista/:id', component: ArtistDetailComponent},
 	{path: 'mis-datos', component: UserEditComponent},
-	{path: '**', component: HomeComponent}
+	{path: '**', component: HomeComponent},
 ];
 
 export const appRoutingProviders: any[] = [];
